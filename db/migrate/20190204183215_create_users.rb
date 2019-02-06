9bb5031c1f64492d93020e398c2028791683d9dd
kokogakayui <kokogakayui@gmail.com>
2019-02-06 19:22:56 +0100

    ok

--- db/migrate/20190204183215_create_users.rb
+++ db/migrate/20190204183215_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.belongs_to :city
       t.string :first_name
       t.string :last_name
-      t.string :username
+      t.string :username, presence: true
       t.integer :age
       t.string :email
       t.text :description
