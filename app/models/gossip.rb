a14f622a686fa5234eb4c0c8028349c297435bb5
smelldat <valentin.cluzan33@gmail.com>
2019-02-08 03:09:18 +0100

    first merge

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,7 +1,7 @@
 class Gossip < ApplicationRecord
   belongs_to :user
   has_many :comments
-  validates :title,  length: {minimum: 3, maximum: 14, too_short: "Il faut plus de 3 caractères dans ton titre !", too_long: "Tu dois écrire un titre de moins de 14 caractères !"   }
+  validates :title,  length: {minimum: 3, maximum: 35, too_short: "Il faut plus de 3 caractères dans ton titre !", too_long: "Tu dois écrire un titre de moins de 25 caractères !"   }
 
   validates_presence_of :title, :message => "Tu as oublié le titre, bébé"
   validates_presence_of :content, :message => "Tu as oublié le contenu du potin, bébé"
